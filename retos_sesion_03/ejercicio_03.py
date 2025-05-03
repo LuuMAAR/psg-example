2b5e816ed64f5d40128277b1688ae354ebe3b62c
Mar <luzmarianabernalbenavides@gmail.com>
2025-05-03 17:21:49 -0400

    Como lavar platos

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,14 @@
+# Tutorial para lavar platos 
+
+print("TUTORIAL PARA LAVAR LOS PLATOS\n")
+print("1. Preparación:")
+print("- Quita restos de comida.")
+print("- Llena el fregadero con agua caliente.\n")
+print("2. Ponerle jabon a la Esponja que tiene un lado suave y otro mas rasposo\n")
+print("3. Orden de lavado: \n ")
+print("   a) Vasos y copas\n   b) Cubiertos\n   c) Platos\n   d) Ollas y sartenes\n")
+print("4. Lavado:")
+print("- Frota con esponja y enjuaga con agua caliente hasta quitar todo el jabón.\n")
+print("5. Secado:")
+print("- Deja secar al aire o seca con un trapo limpio.\n")
+
