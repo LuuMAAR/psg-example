9332f1ee6a0eaf11a1a644a79294ae8806a8d8ad
Mar <luzmarianabernalbenavides@gmail.com>
2025-05-28 12:02:14 -0400

    Tarjeta y huella

---
+++ retos_sesion_06/ejercicio_03.py
@@ -0,0 +1,7 @@
+print("Tarjeta | Huella | Puerta se abre")
+print("-------------------------------")
+
+print(False, "|", False, "|", False or False)
+print(False, "|", True,  "|", False or True)
+print(True,  "|", False, "|", True or False)
+print(True,  "|", True,  "|", not(True or True))
