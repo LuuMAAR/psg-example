21e2cfcf36814e29ba148ce45b32bcac0b2e707b
Mar <luzmarianabernalbenavides@gmail.com>
2025-05-28 12:11:48 -0400

    Par impar

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,15 @@
+a=44
+b=98
+ra=44%2
+rb=98%2
+ba=bool(ra)
+bb=bool(rb)
+print("True es Par False es impar")
+print(f"{a} es {not ba} y {b} es {not bb} ")
+a1=111
+b1=333
+ra1=44%2
+rb1=98%2
+ba1=bool(ra1)
+bb1=bool(rb1)
+print(f"{a1} es {ba1} y {b1} es {bb1} ")
